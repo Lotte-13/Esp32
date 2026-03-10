58aecf4a25e3c854cb8957467e12c3cf8b7f419e
Lotte <Lotte.vankevelaer@student.be>
2026-03-10 14:01:16 +0100

    labo

--- project_DAC_LEDC/main/main.c
+++ project_DAC_LEDC/main/main.c
@@ -1,9 +1,20 @@
-#include <stdio.h>"
+#include <stdio.h>
 #include "myGPIO.h"
 #include "myADC.h"
 
+#define KNOP_PIN 2
+#define LED_PIN 1
+#define pot 0
 
-void app_main(void)
-{ 
+void main() 
+{
+
+}
+
+    int getal = myADC_getValue(pot)*10/1074;
+
+    if (myGPIO_KNOP_GetFlank(KNOP_PIN)){
+        
+    }
     
 }
